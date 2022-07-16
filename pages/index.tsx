93506492c1300aa089caad2a322e65e64881695e
Edward Taylor <edeetee@gmail.com>
2022-07-17 01:27:15 +1200

    favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,10 @@ const Home: React.FC = () => {
     return <>   
         <Head>
             <title>Edward Taylor</title>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0"/> 
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <link rel="icon" type="image/png" href="/favicon/16.png"></link>
+            <link rel="icon" sizes="32x32" type="image/png" href="/favicon/32.png"></link>
+            <link rel="icon" sizes="64x64" type="image/png" href="/favicon/64.png"></link>
         </Head> 
         <CanvasRenderer />
         <Main />
