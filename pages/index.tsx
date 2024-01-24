6498df3ea71e52affecbf4ffcfb634a3dd7a9378
Edward <edeetee@gmail.com>
2024-01-24 20:01:01 +1300

    fix crash on some devices

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,21 +3,23 @@ import React from "react";
 import { CanvasRenderer } from "@src/lib/CanvasRenderer";
 import Head from "next/head";
 import { Main } from "@components/css/main";
+import { FeedbackRenderer } from "@src/lib/feedbackRenderer";
 
 const Home: React.FC = () => {
-    return <>   
+    return <>
         <Head>
             <title>Edward Taylor</title>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <link rel="icon" type="image/png" href="/favicon/16.png"></link>
             <link rel="icon" sizes="32x32" type="image/png" href="/favicon/32.png"></link>
             <link rel="icon" sizes="64x64" type="image/png" href="/favicon/64.png"></link>
             <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-        </Head> 
-        <CanvasRenderer />
+        </Head>
+
+        <CanvasRenderer buildRenderer={FeedbackRenderer} style={{ backgroundColor: 'black', position: 'fixed', zIndex: -1 }} />
         <Main />
         {/* <AnimatedMe /> */}
-    </>  
+    </>
 };
 
 export default Home;
--- src/lib/CanvasRenderer.tsx
+++ src/lib/CanvasRenderer.tsx
@@ -1,100 +1,157 @@
-
-//TODO: maybe make component class?
-
 import React, { useEffect } from "react";
-import REGL from "regl";
-import { FeedbackRenderer } from "src/lib/feedbackRenderer";
-import { constrain } from "src/lib/constrain";
+import REGL, { Regl } from "regl";
+import vec from "fast-vector";
+import { constrain } from "./constrain";
 
-const RES_MULTIPLIER = 1 / 2
+const RES_MULTIPLIER = 1 / 2;
 
-export const CanvasRenderer = () => {
-    const canvasRef = React.createRef<HTMLCanvasElement>()
+export interface MouseMoveListener {
+    (x: number, y: number): void;
+}
+
+export interface PressListener {
+    (pressed: boolean): void;
+}
+
+export type BuildRenderer = (regl: Regl) => {
+    onFrame: (ctx: REGL.DefaultContext) => void;
+    onMove?: (x: number, y: number) => void;
+    onPress?: (pressed: boolean) => void;
+};
+
+export class SmoothedMouse {
+    private target: vec = new vec(0, 0);
+    private current: vec = new vec(0, 0);
+    uninitialized = true;
+
+    get pos() {
+        return this.current;
+    }
+
+    get rawPos() {
+        return this.target;
+    }
+
+    get diff() {
+        return this.target.sub(this.current);
+    }
+
+    setMousePos(new_target: vec) {
+        this.target = new_target;
+        if (this.uninitialized) {
+            this.current = this.target;
+            this.uninitialized = false;
+        }
+    }
+
+    update(limit: number, aspect = 1.0, mult = 1.0) {
+        const diff = vec.div(this.diff.mul(mult).limit(limit), aspect, 1);
+        this.current = this.current.add(diff);
+    }
+}
+
+type CanvasRendererProps = { buildRenderer: BuildRenderer } & React.DetailedHTMLProps<
+    React.CanvasHTMLAttributes<HTMLCanvasElement>,
+    HTMLCanvasElement
+>;
+
+function makeRendererOnCanvas(
+    canvasRef: React.RefObject<HTMLCanvasElement>,
+    buildRenderer: BuildRenderer,
+) {
+    if (window === undefined) return;
+
+    const canvas = canvasRef.current;
+
+    const glContext = canvas?.getContext("webgl") || undefined;
+
+    function updateSize() {
+        if (canvas && glContext) {
+            canvas.width = window.innerWidth * RES_MULTIPLIER;
+            canvas.height = window.innerHeight * RES_MULTIPLIER;
+            glContext.viewport(0, 0, canvas.width, canvas.height);
+        }
+    }
+
+    window.addEventListener("resize", updateSize);
+
+    updateSize();
+
+    const regl = REGL({
+        // pixelRatio: 0.001,
+        // canvas: canvas,
+        gl: glContext,
+        extensions: [
+            "OES_texture_float",
+            // 'GL_OES_texture_float',
+            "WEBGL_color_buffer_float",
+            "OES_texture_float_linear",
+            // 'WEBGL_texture_float'
+        ],
+        optionalExtensions: ["EXT_color_buffer_float"],
+        // optionalExtensions: ['OES_texture_float_linear', "WEBGL_color_buffer_float"],
+        onDone(err) {
+            if (err != null) console.log(err);
+        },
+    });
+
+    const renderer = buildRenderer(regl);
+
+    function updateMouse(pageX: number, pageY: number) {
+        const x = constrain((pageX - window.scrollX) / window.innerWidth, 0, 1);
+        const y = constrain(1 - (pageY - window.scrollY) / window.innerHeight, 0, 1);
+
+        if (renderer.onMove) renderer.onMove(x, y);
+    }
+
+    function onPress(pressed: boolean) {
+        if (renderer.onPress) renderer.onPress(pressed);
+    }
+
+    document.body.addEventListener("touchmove", (ev) =>
+        updateMouse(ev.touches[0].pageX, ev.touches[0].pageY),
+    );
+    document.body.addEventListener("mousemove", (ev) => updateMouse(ev.pageX, ev.pageY));
+    document.body.addEventListener("mouseover", (ev) => updateMouse(ev.pageX, ev.pageY));
+
+    document.body.addEventListener("mousedown", () => onPress(true));
+    document.body.addEventListener("touchstart", () => onPress(true));
+
+    document.body.addEventListener("touchend", () => onPress(false));
+    document.body.addEventListener("touchcancel", () => onPress(false));
+    document.body.addEventListener("mouseup", () => onPress(false));
+
+    regl.frame((ctx) => {
+        renderer.onFrame(ctx);
+    });
+}
+
+export const CanvasRenderer = ({ buildRenderer, ...props }: CanvasRendererProps) => {
+    const canvasRef = React.createRef<HTMLCanvasElement>();
+
+    const { style, ...otherProps } = props;
 
     useEffect(() => {
-        if (window === undefined)
-            return
-
-        const canvas = canvasRef.current
-
-        const glContext = canvas?.getContext("webgl") || undefined
-
-        function updateSize() {
-
-            if (canvas && glContext) {
-                const newWidth = window.innerWidth * RES_MULTIPLIER;
-                const newHeight = window.innerHeight * RES_MULTIPLIER;
-                if (canvas.width < newWidth || canvas.height < newHeight) {
-                    canvas.width = newWidth
-                    canvas.height = newHeight
-                    glContext.viewport(0, 0, canvas.width, canvas.height)
-                }
-            }
+        try {
+            makeRendererOnCanvas(canvasRef, buildRenderer);
+        } catch (e) {
+            console.error(e);
         }
+    });
 
-        window.addEventListener("resize", updateSize)
-
-        updateSize()
-
-        const regl = REGL({
-            // pixelRatio: 0.001,
-            // canvas: canvas,
-            gl: glContext,
-            extensions: [
-                'OES_texture_float',
-                // 'GL_OES_texture_float', 
-                'WEBGL_color_buffer_float',
-                'OES_texture_float_linear'
-                // 'WEBGL_texture_float'
-            ],
-            optionalExtensions: [
-                'EXT_color_buffer_float'
-            ],
-            // optionalExtensions: ['OES_texture_float_linear', "WEBGL_color_buffer_float"],
-            onDone(err) {
-                if (err != null)
-                    console.log(err)
-            }
-        })
-
-        const feedbackRenderer = FeedbackRenderer(regl)
-
-        function updateMouse(pageX: number, pageY: number) {
-            if (canvasRef.current == null)
-                return;
-
-            const x = constrain((pageX - window.scrollX) / canvasRef.current.clientWidth, 0, 1)
-            const y = constrain(1 - (pageY - window.scrollY) / window.innerHeight, 0, 1)
-
-            feedbackRenderer.onMove(x, y)
-        }
-
-        document.addEventListener("touchmove", ev => updateMouse(ev.touches[0].pageX, ev.touches[0].pageY))
-        document.addEventListener("mousemove", ev => updateMouse(ev.pageX, ev.pageY))
-        document.addEventListener("mouseover", ev => updateMouse(ev.pageX, ev.pageY))
-
-        document.addEventListener("mousedown", () => feedbackRenderer.onPress(true))
-        document.addEventListener("touchstart", () => feedbackRenderer.onPress(true))
-
-        document.addEventListener("touchend", () => feedbackRenderer.onPress(false))
-        document.addEventListener("touchcancel", () => feedbackRenderer.onPress(false))
-        document.addEventListener("mouseup", () => feedbackRenderer.onPress(false))
-
-        regl.frame(ctx => {
-            feedbackRenderer.onFrame(ctx)
-        })
-    })
-
-    return <canvas ref={canvasRef}
-        style={{
-            imageRendering: "pixelated",
-            position: "fixed",
-            height: "100vh",
-            width: '100vw',
-            left: 0,
-            top: 0,
-            background: "black",
-            zIndex: -1
-        }}
-    ></canvas>
-}
+    return (
+        <canvas
+            ref={canvasRef}
+            style={{
+                imageRendering: "pixelated",
+                position: "fixed",
+                width: "100%",
+                height: "100%",
+                left: 0,
+                top: 0,
+                ...style,
+            }}
+            {...otherProps}
+        ></canvas>
+    );
+};
