a7618d3fc064c45409582b16fc16aac901f1143f
Edward <edeetee@gmail.com>
2024-05-27 10:54:48 +1200

    stylesheet doc

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,29 @@
-import Document, { DocumentContext, DocumentInitialProps } from "next/document";
+// import Document, { DocumentContext, DocumentInitialProps } from "next/document";
 
-class CustomDocument extends Document {
-    static async getInitialProps(
-        ctx: DocumentContext,
-    ): Promise<DocumentInitialProps> {
-        const initialProps = await Document.getInitialProps(ctx);
+// class CustomDocument extends Document {
+//     static async getInitialProps(
+//         ctx: DocumentContext,
+//     ): Promise<DocumentInitialProps> {
+//         const initialProps = await Document.getInitialProps(ctx);
 
-        return initialProps;
-    }
+//         return initialProps;
+//     }
+// }
+
+// export default CustomDocument;
+
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function CustomDocument() {
+    return (
+        <Html>
+            <Head>
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=block" />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
 }
-
-export default CustomDocument;
