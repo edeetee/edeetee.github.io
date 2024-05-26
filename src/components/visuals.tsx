6e601fa7af6cdad20c1e48265498332314ac3d98
Edward <edeetee@gmail.com>
2024-05-27 10:59:33 +1200

    remove those videos

--- src/components/visuals.tsx
+++ src/components/visuals.tsx
@@ -17,10 +17,10 @@ export const Events = () =>
                 I forge original visual toolchains with Resolume, Touchdesigner and his own experimental software. His generative works transform stages into dynamic installations, merging seamlessly with the art of musicians and stage designers for a cohesive experience.
                 You can see more of my content if you scroll down or <a href="https://instagram.com/optiphonic">on instagram</a>.
             </p>
-            <div style={{ width: "100%", display: "flex", alignItems: "center", justifyContent: "space-around" }}>
-                <Video style={{ width: 300 }} src="https://user-images.githubusercontent.com/7484745/253841919-00aac186-7d5b-4839-8db7-d66f50b6a360.mp4" />
-                <Video style={{ width: 300 }} src="https://user-images.githubusercontent.com/7484745/253841968-ac6e9e32-4514-4d5e-bcee-56ed53065d3e.mp4" />
-            </div>
+            {/* <div style={{ width: "100%", display: "flex", alignItems: "center", justifyContent: "space-around" }}>
+                <Video style={{ maxWidth: 300, aspectRatio: '1' }} src="https://user-images.githubusercontent.com/7484745/253841919-00aac186-7d5b-4839-8db7-d66f50b6a360.mp4" />
+                <Video style={{ maxWidth: 300, aspectRatio: '1' }} src="https://user-images.githubusercontent.com/7484745/253841968-ac6e9e32-4514-4d5e-bcee-56ed53065d3e.mp4" />
+            </div> */}
 
             <p>
 
