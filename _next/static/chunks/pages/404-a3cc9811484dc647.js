b44343a921ea3d25b4b3b8a7302038ab7354b426
edeetee <edeetee@users.noreply.github.com>
2025-04-22 10:15:53 +0000

    deploy: 9c5703382926642d962e8c57663bce552aac4e20

---
+++ _next/static/chunks/pages/404-a3cc9811484dc647.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[197],{6141:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/404",function(){return _(6767)}])},6767:function(n,u,_){"use strict";_.r(u);var t=_(1118);u.default=t.default}},function(n){n.O(0,[702,511,463,118,888,774,179],function(){return n(n.s=6141)}),_N_E=n.O()}]);
